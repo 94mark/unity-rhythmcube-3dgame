7540d5ee47adff68cdf1fcf47cc3f69da9f65883
94mark <90877724+94mark@users.noreply.github.com>
2022-03-31 23:58:35 +0900

    Update

---
+++ rhythmcube/Assets/Scripts/CenterFrame.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterFrame : MonoBehaviour
+{
+    AudioSource myAudio;
+    bool musicStart = false;
+
+    private void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!musicStart)
+        {
+            if (collision.CompareTag("Note"))
+            {
+                myAudio.Play();
+            }
+        }
+    }
+}
